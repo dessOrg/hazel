ed43639c02b68dcba62b2a4fc062d48f005fb397
dessHub <deskip95@gmail.com>
2017-07-13 22:52:45 +0300

    initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -136,9 +136,11 @@
             </div>
         </nav>
 
+@yield('content')
+
   </div>
 
-    @yield('content')
+
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
