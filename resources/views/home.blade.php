b843d6ab7e1468fe48e740046eac8a5bbc0e9b2d
dessHub <deskip95@gmail.com>
2017-07-13 22:47:56 +0300

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="content">
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-md-1"> <!-- Logo section -->
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
+
+      </div>
+      <div class="col-md-11"> <!-- Body section -->
+
+        <div class="col-md-9 title"> <!-- Title section -->
+          <h3>Supliment your income with Akiba Account.</h3>
+
         </div>
+        <div class="col-md-3"> <!-- Time section -->
+       <i class="fa fa-clock-o"></i> 2 days ago
+
+        </div>
+        <div class="col-md-12">
+         <p>Supliment your income with Akiba Account.</p>
+        </div>
+
+        <div class="col-md-offset-10 col-md-2">
+          <button type="submit" class="btn btn-warning btn-fill pull-right">Learn More</button>
+        </div>
+
+      </div>
+    </div>
     </div>
 </div>
 @endsection
