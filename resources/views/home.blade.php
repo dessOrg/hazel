282645aa2e63f9722012c1fc8c762856139553b2
dessHub <deskip95@gmail.com>
2017-07-13 23:22:22 +0300

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,6 +31,33 @@
       </div>
     </div>
     <hr>
+    <div class="row">
+    <div class="col-md-1" style="padding-top:30px;"> <!-- Logo section -->
+
+  <i class="fa fa-circle fa-5x text-warning"></i>
+
+    </div>
+    <div class="col-md-11"> <!-- Body section -->
+
+      <div class="col-md-9 title"> <!-- Title section -->
+        <h3><b>Supliment your income with Akiba Account.</b></h3>
+
+      </div>
+      <div class="col-md-3"> <!-- Time section -->
+     <i class="fa fa-clock-o"></i> 2 days ago
+
+      </div>
+      <div class="col-md-12">
+       <p>Supliment your income with Akiba Account.</p>
+      </div>
+
+      <div class="col-md-offset-10 col-md-2">
+        <button type="submit" class="btn btn-warning btn-fill pull-right">Learn More</button>
+      </div>
+
+    </div>
+  </div>
+  <hr>
     </div>
 </div>
 @endsection
