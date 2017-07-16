d2caeb060a1a796b2a71aa17fa8ca53e8dfa7217
dessHub <deskip95@gmail.com>
2017-07-16 09:55:54 +0300

    redesigning index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,13 +47,13 @@
           <a class="navbar-brand" href="#">WebSiteName</a>
         </div>
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">Page 1</a></li>
-          <li><a href="#">Page 2</a></li>
+          <li class="active col-xs-1"><a href="#">Home</a></li>
+          <li class="col-xs-1"><a href="#">Page 1</a></li>
+          <li class="col-xs-1"><a href="#">Page 2</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          <li class="col-xs-1"><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li class="col-xs-1"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
         </ul>
       </div>
     </nav>
