1d63fdc960614a7793556d696ce196efbb9de3fd
dessHub <deskip95@gmail.com>
2017-07-16 11:19:39 +0300

    redesigning index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,28 +6,28 @@
       <hr>
       @foreach($opportunities as $key)
       <div class="row">
-      <div class="col-md-1" style="padding-top:30px;"> <!-- Logo section -->
+      <div class="col-md-1 col-xs-offset-4  col-xs-3" style="padding-top:30px;"> <!-- Logo section -->
 
     <i class="fa fa-circle fa-5x text-info"></i>
 
       </div>
-      <div class="col-md-11"> <!-- Body section -->
+      <div class="col-md-11 col-xs-12"> <!-- Body section -->
 
-        <div class="col-md-9 title"> <!-- Title section -->
+        <div class="col-md-9 col-xs-12 title"> <!-- Title section -->
           <h3><b>{{$key->title}}.</b></h3>
 
         </div>
-        <div class="col-md-3"> <!-- Time section -->
+        <div class="col-md-3 col-xs-12"> <!-- Time section -->
        <i class="fa fa-clock-o"></i> {{$key->created_at}}
 
         </div>
-        <div class="col-md-12">
+        <div class="col-md-12 col-xs-12">
          <p>{{$key->description}}.</p>
         </div>
-        <div class="col-md-10">
+        <div class="col-md-10 col-xs-12">
           <p><i class="fa pe-7s-star"></i><em>{{$key->company_name}}</em></p>
         </div>
-        <div class="col-md-2">
+        <div class="col-md-2 col-xs-12">
           <button type="submit" class="btn btn-warning btn-fill pull-right">Learn More</button>
         </div>
 
