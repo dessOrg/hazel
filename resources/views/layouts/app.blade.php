9077cf42eceee8514dd556d4db607f7158f6a52d
dessHub <deskip95@gmail.com>
2017-07-17 10:51:30 +0300

    cdn link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Coinbox</title>
+    <!-- Fonts -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+  <!-- Styles -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+  {{-- <link href="{{ elixir('css/style.css') }}" rel="stylesheet"> --}}
      <link href="/assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
 
 
